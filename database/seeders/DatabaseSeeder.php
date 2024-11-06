420945d1c8b1568c2c1fe09f6f7cc71af46d6246
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-11-06 15:27:20 +0100

    error fixed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,9 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call([
+            TypeSeeder::class,
             ProjectSeeder::class,
+           
         ]);
     }
 }
--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Database\Seeders;
-
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +12,16 @@ class TypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+    $typeName=[
+        "Front-end",
+        "Back-end",
+        "Full-stack",
+        "Database",
+    ];
+    foreach($typeName as $name) {
+        $newType= new Type();
+        $newType->name=$name;
+        $newType->save();
+    }
     }
 }
