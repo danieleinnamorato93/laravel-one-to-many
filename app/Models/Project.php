05dff46196bb97a48bf766ad95edce78f6eebe57
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-11-06 18:14:11 +0100

    It doesn't work

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -11,4 +11,8 @@ class Project extends Model
     protected $fillable  = [
         "title","content","url","type_id",
     ];
+    
+    public function category(){
+        return $this->belongsTo(Type::class);
+    }
 }
--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -7,7 +7,7 @@ use App\Models\Project;
 use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Faker\Factory as Faker;
+use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
 {
@@ -17,39 +17,45 @@ class ProjectSeeder extends Seeder
     public function run(Faker $faker): void
     {
 
-        $types= Type::all()->pluck("id");
+      
 
-            $projectsData= [
-                ["title"=>"Spotify Web",
-                "content"=>"Un progetto di replica di Spotify con HTML e CSS ho riprodotto l'aspetto della popolare piattaforma di streaming musicale.",
-                "url"=>"https://github.com/danieleinnamorato93/html-css-spotifyweb",
-                "type_id"=>$faker->randomElement($types),
-                ],
-                ["title"=>"Boolando",
-                "content"=>"Un progetto di replica del sito di Zalando, con l'uso di HTML/CSS.",
-                "url"=>"https://github.com/danieleinnamorato93/html-css-boolando",
-                "type_id"=>$faker->randomElement($types),
-                ],
-                ["title"=>"Vue-Boolzapp",
-                "content"=>"Un progetto di replica dell'applicazione di whatsapp, mediante l'uso del framework Vue.Js",
-                "url"=>"https://github.com/danieleinnamorato93/vue-boolzapp",
-                "type_id"=>$faker->randomElement($types),
-                ],
-                ["title"=>"Js-Campominato-Dom",
-                "content"=>"Mi sono divertito a ricreare il gioco del Campominato attraverso ll'utilizzo di Javascript.",
-                "url"=>"https://github.com/danieleinnamorato93/js-campominato-dom",
-                "type_id"=>$faker->randomElement($types),
-                ],
-                ["title"=>"Php-Badwords",
-                "content"=>"Simulazione di un applicativo che permette di censurare qualsiasi parola si desideri, eseguito con PHP.",
-                "url"=>"https://github.com/danieleinnamorato93/php-badwords",
-                "type_id"=>$faker->randomElement($types),
+        $projectsData= [
+            ["title"=>"Spotify Web",
+            "content"=>"Un progetto di replica di Spotify con HTML e CSS ho riprodotto l'aspetto della popolare piattaforma di streaming musicale.",
+            "url"=>"https://github.com/danieleinnamorato93/html-css-spotifyweb",
+            
+            ],
+            ["title"=>"Boolando",
+            "content"=>"Un progetto di replica del sito di Zalando, con l'uso di HTML/CSS.",
+            "url"=>"https://github.com/danieleinnamorato93/html-css-boolando",
+            
+            ],
+            ["title"=>"Vue-Boolzapp",
+            "content"=>"Un progetto di replica dell'applicazione di whatsapp, mediante l'uso del framework Vue.Js",
+            "url"=>"https://github.com/danieleinnamorato93/vue-boolzapp",
+            
+            ],
+            ["title"=>"Js-Campominato-Dom",
+            "content"=>"Mi sono divertito a ricreare il gioco del Campominato attraverso ll'utilizzo di Javascript.",
+            "url"=>"https://github.com/danieleinnamorato93/js-campominato-dom",
+            
+            ],
+            ["title"=>"Php-Badwords",
+            "content"=>"Simulazione di un applicativo che permette di censurare qualsiasi parola si desideri, eseguito con PHP.",
+            "url"=>"https://github.com/danieleinnamorato93/php-badwords",            
             ],
 
-];
-foreach ($projectsData as $project) {
-    Project::create($project);
-    }
+        ];
+        $types= Type::all()->pluck("id");
+        foreach ($projectsData as $project) {
+            $newProject= new Project();
+            $newProject->type_id = $faker->randomElement($types);
+            $newProject->title = $project['title'];
+            $newProject->content = $project['content'];
+            $newProject->url = $project['url'];
+            $newProject->save();
+
+        }
        
     }
 }
